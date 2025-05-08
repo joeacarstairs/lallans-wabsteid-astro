ab436fb120cbf5c2bf16788bbf6168a36e88ab16
Joe Carstairs <me@joeac.net>
2025-05-08 11:55:05 +0100

    Copyright statement

--- src/i18n/translations/pages/submit.ts
+++ src/i18n/translations/pages/submit.ts
@@ -25,11 +25,13 @@ const tPage = {
   },
   'submit-para-2': {
     sco: () => `
-      Send us your cuttie tales, poesie and drama til
+      Read the copyricht statement ablo. Gin you're happy wi thon, send us your
+      cuttie tales, poesie and drama til
       <a href="mailto:lallans@hotmail.co.uk">lallans@hotmail.co.uk</a>.
     `,
     'en-GB': () => `
-      Send us your short stories, poetry and drama to
+      Read the copyright statement below. If you're happy with it, send us your
+      short stories, poetry and drama to
       <a href="mailto:lallans@hotmail.co.uk">lallans@hotmail.co.uk</a>.
     `,
   },
@@ -41,6 +43,19 @@ const tPage = {
     'en-GB': () => `
       We look forward to having your submission, whether you’ve been
       submitting for years or you’re a first-time writer!
+    `,
+  },
+  'copyright-heading': {
+    sco: () => 'Copyricht statement',
+    'en-GB': () => 'Copyright statement',
+  },
+  'copyright-body': {
+    sco: () => `
+      Pieces submitted to Lallans, should be the original work of the person
+      submitting them. Copyright remains with the author, however, submitting a
+      piece for inclusion gives the Scots Language Society, though Lallans or
+      other title, the right, in perpetuity, to republish the work in hard copy
+      or digital form and to reformat it as necessary.
     `,
   },
 };
